ad706d3d535f7cbf9a4174c61099e5941fc69338
0x00991 <129187044+0x00991@users.noreply.github.com>
2023-08-02 00:36:05 +0900

    알림창이 정상적으로 표시되지 않는 문제, 프로세스 감지 시 시작 알림과 중복 알림이 함께 뜨는 문제

--- reminder-background.py
+++ reminder-background.py
@@ -1,4 +1,5 @@
 import datetime
+from tkinter import Tk
 from tkinter.messagebox import showinfo
 from threading import Thread
 from shared import *
@@ -11,11 +12,31 @@ check_dir()
 
 ts = lambda: datetime.datetime.now().timestamp()
 
-threads = []
+# threads = []
+
+
+alertqueue = [] # [[title, message]]
+def alertthread():
+    root = Tk()
+    root.wm_attributes("-topmost", 1)
+    root.withdraw()
+    while run or alertqueue:
+        for a in alertqueue:
+            showinfo(title=a[0], message=a[1], parent=root)
+        alertqueue.clear()
+        time.sleep(1)
+
+# def show(title, message):
+#     # showinfo(title=title, message=message)
+#     showinfo(title=title, message=message, parent=root)
+    
+# def alert(title: str = "Reminder", message: str = "리마인드 메세지", join=False):
+#     thread = Thread(target=show, kwargs={"title": title, "message": message}, daemon=True)
+#     thread.start()
+#     if join:
+#         threads.append(thread)
 def alert(title: str = "Reminder", message: str = "리마인드 메세지"):
-    thread = Thread(target=showinfo, kwargs={"title": title, "message": message}, daemon=True)
-    thread.start()
-    threads.append(thread)
+    alertqueue.append([title, message])
 
 
 config = {}
@@ -41,52 +62,69 @@ def to_int(string):
     except:
         return -1
 
-def task():
-    if not os.path.isfile(ROOT_DIR+"/DISABLEALERT"):
-        alert("실행 알림", "리마인더 백그라운드 프로세스가 시작되었습니다. 함께 배포된 remove-alert.exe 파일을 실행해 알림을 제거할 수 있습니다.")
+def preload():
     global run
-    while run:
-        if os.path.isfile(ROOT_DIR+"/running.txt"):
-            with open(ROOT_DIR+"/running.txt", "r", encoding="utf-8") as f:
-                
-                try:
-                    fr = json.loads(f.read())
-                    if fr[0]+config.get("interval", DEFAULT_INTERVAL)-0.1 >= ts():
-                        alert("실행 알림", f"리마인더 백그라운드 프로세스의 작동이 감지되었습니다. 이 알림이 오류라고 생각하신다면 {config.get('interval', DEFAULT_INTERVAL)}초 후 프로세스를 다시 실행해주세요.") 
-                        run = False
-                        break
-                except:
-                    pass
-        with open(ROOT_DIR+"/running.txt", "w", encoding="utf-8") as f:
-            f.write(json.dumps([ts(), os.getpid()]))
-
+    while True:
         if not config and not load_config():
             time.sleep(1)
             continue
+        else:
+            break
         
-        check_dir()
-        if not os.listdir(ALERT_DIR):
-            time.sleep(config.get("interval", DEFAULT_INTERVAL))
-            continue
+    if os.path.isfile(ROOT_DIR+"/running.txt"):
+        with open(ROOT_DIR+"/running.txt", "r", encoding="utf-8") as f:
+            try:
+                fr = json.loads(f.read())
+                if fr[0]+config.get("interval", DEFAULT_INTERVAL)-0.1 >= ts():
+                    print(True)
+                    alert("실행 알림", f"리마인더 백그라운드 프로세스의 작동이 감지되었습니다. 이 알림이 오류라고 생각하신다면 {config.get('interval', DEFAULT_INTERVAL)}초 후 프로세스를 다시 실행해주세요.") 
+                    run = False
+                    return
+            except: pass
+            
+    if not os.path.isfile(ROOT_DIR+"/DISABLEALERT"):
+        alert("실행 알림", "리마인더 백그라운드 프로세스가 시작되었습니다. 함께 배포된 remove-alert.exe 파일을 실행해 알림을 제거할 수 있습니다.")
+
+def task():
+    global run
         
-        times = list(map(to_int, os.listdir(ALERT_DIR)))
-        # past = []
-        timestamp = ts()
-        for t in times:
-            if t < timestamp: # 과거라면
-                # past.append(time)
-                with open(f"{ALERT_DIR}/{t}", "rb") as f:
-                    content = f.read().decode("utf-8")
-                os.remove(f"{ALERT_DIR}/{t}")
-                alert(f"리마인더 - 지금으로부터 {round(timestamp - t)}초 전..", content)
-                print(f"alert - {t} ({content})")
-        time.sleep(config.get("interval", DEFAULT_INTERVAL))
+    if os.path.isfile(ROOT_DIR+"/running.txt"):
+        with open(ROOT_DIR+"/running.txt", "r", encoding="utf-8") as f:
+            try:
+                fr = json.loads(f.read())
+                if fr[0]+config.get("interval", DEFAULT_INTERVAL)-0.1 >= ts():
+                    alert("실행 알림", f"리마인더 백그라운드 프로세스의 작동이 감지되었습니다. 이 알림이 오류라고 생각하신다면 {config.get('interval', DEFAULT_INTERVAL)}초 후 프로세스를 다시 실행해주세요.") 
+                    run = False
+                    return
+            except: pass
+
                 
+    with open(ROOT_DIR+"/running.txt", "w", encoding="utf-8") as f:
+        f.write(json.dumps([ts(), os.getpid()]))
+
+    check_dir()
+    if not os.listdir(ALERT_DIR): return
+        
+    times = list(map(to_int, os.listdir(ALERT_DIR)))
+    timestamp = ts()
+    for t in times:
+        if t < timestamp: # 과거라면
+            with open(f"{ALERT_DIR}/{t}", "rb") as f:
+                content = f.read().decode("utf-8")
+            os.remove(f"{ALERT_DIR}/{t}")
+            alert(f"리마인더 - 지금으로부터 {round(timestamp - t)}초 전..", content)
+            print(f"alert - {t} ({content})")
                 
+ath = Thread(target=alertthread, daemon=True)
+preload()
+ath.start()
 while run:
     try:
         task()
+        time.sleep(config.get("interval", DEFAULT_INTERVAL))
     except KeyboardInterrupt:
         run = False
 
-for th in threads: th.join()
+ath.join()
+# for t in threads:
+    # t.join()
